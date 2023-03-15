2909c911abf641d18167c90c16d064764d82369a
William Hill <williamhill@Williams-MacBook-Pro.local>
2023-03-15 15:01:02 -0400

    finished exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(convert) {
+  let num = (convert - 32) * 5/9 
+  return Math.round(num * 10) / 10
+ 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(convert) {
+  let num = (convert * 9/5 + 32)
+  return Math.round(num * 10) / 10
 };
 
 // Do not edit below this line
